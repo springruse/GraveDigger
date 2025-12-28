69f927b6a2711ee8b143c0b54fca678d94ffb3df
springruse <152939330+springruse@users.noreply.github.com>
2025-12-28 04:21:58 -0700

    Add project files.

---
+++ Source/GraveDigger/GraveDiggerMod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using HarmonyLib;
+using Verse;
+
+namespace GraveDigger
+{
+    public class GraveDiggerMod : Mod
+    {
+        public GraveDiggerMod(ModContentPack content) : base(content)
+        {
+            Log.Message("GraveDigger mod loaded!");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# GraveDigger
+A RimWorld mod inspired by the Roblox game GraveDigger.
+
+## Features
+- Mining pickaxes with combat capabilities
+- (More coming soon!)
+
+## Installation
+Download and extract to your RimWorld Mods folder.
